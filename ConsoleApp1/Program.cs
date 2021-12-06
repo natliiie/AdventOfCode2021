b35af13fb49f73c1f2ce5147e3b420c0cc0442a5
Natalie Nilsson <natalie.nilsson2@learnet.se>
2021-12-06 20:42:35 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.ExceptionServices;
+using Kalender;
+
+internal class Program
+{
+
+
+    internal static void Main()
+    {
+        var bla = new Class1();
+
+        bla.three_one();
+
+    }
+
+    
+
+}
